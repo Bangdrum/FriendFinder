f8772e26f33d25dfde72eed03cd985e8c0f79769
Ryan Shepard <bangdrumshep@gmail.com>
2020-04-22 18:41:04 -0700

    complete

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friendsData = [{
+        name: "Rachel Brosnahan",
+        photo: "https://pmcdeadline2.files.wordpress.com/2018/10/rexfeatures_9911491be.jpg?w=681&h=383&crop=1 ",
+        scores: [5, 3, 4, 2, 3, 5, 3, 4, 2, 3],
+    },
+    {
+        name: "Michael Zegen",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Michael_Zegen_%28Mad_Men%29.jpg ",
+        scores: [3, 4, 1, 1, 4, 5, 3, 4, 2, 3],
+    },
+    {
+        name: " Alex Borstein",
+        photo: "https://en.wikipedia.org/wiki/Alex_Borstein#/media/File:Alex_Borstein_by_Gage_Skidmore_4_(retouched).jpg ",
+        scores: [2, 2, 2, 2, 2, 5, 3, 4, 2, 3],
+    },
+    {
+        name: "Luke Kirby",
+        photo: "https://en.wikipedia.org/wiki/Luke_Kirby#/media/File:Luke_Kirby_in_2016.jpg ",
+        scores: [4, 5, 5, 3, 3, 5, 3, 4, 2, 3],
+    },
+    {
+        name: "Marin Hinkle",
+        photo: "https://en.wikipedia.org/wiki/Marin_Hinkle#/media/File:MarinHinkleHWOFSept2011.jpg ",
+        scores: [1, 1, 3, 1, 1, 5, 3, 4, 2, 3],
+    },
+    {
+        name: "Tony Shalhoub",
+        photo: "https://en.wikipedia.org/wiki/Tony_Shalhoub#/media/File:Tony_Shalhoub_2017_(cropped).jpg ",
+        scores: [3, 2, 4, 3, 5, 5, 3, 4, 2, 3],
+    },
+];
+
+module.exports = friendsData;
